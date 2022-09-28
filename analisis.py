c3b21a33fde0a1133fcf6d105028a1e1595818f9
505 <505@academicos.cesde.edu.co>
2022-09-28 17:34:31 -0500

    Avance del proyecto 28-09-2022

--- analisis.py
+++ analisis.py
@@ -1,3 +1,4 @@
+from statistics import median
 import pandas as pd
 
 tablaSiembras=pd.read_csv('./Siembras.csv')
@@ -30,12 +31,72 @@ tablaSiembras=pd.read_csv('./Siembras.csv')
 #archivoTEXTO.write(archivoHTML) #agregar el archivo de texto el archivo que creamos
 #archivoTEXTO.close() #cerrar el archivo de texto 
 
-tablaDatosxMunicipio=tablaSiembras[(tablaSiembras ['Ciudad'] =="Andes") | (tablaSiembras ['Ciudad'] =="Barbosa") | (tablaSiembras ['Ciudad'] =="Caldas") | (tablaSiembras ['Ciudad'] =="Tamesis")| (tablaSiembras ['Ciudad'] =="Yarumal")]
 
-archivoHTML=tablaDatosxMunicipio.to_html()
-archivoTEXTO=open("Datosxmunicipio.html","w")
-archivoTEXTO.write(archivoHTML) 
-archivoTEXTO.close() 
+#Datos por municipio:
+#-Andes
+#-Barbosa
+#-Caldas
+#-Támesis
+#-Yarumal
+#tablaDatosxMunicipio=tablaSiembras[(
+#  | (tablaSiembras ['Ciudad'] =="Barbosa") | (tablaSiembras ['Ciudad'] =="Caldas") | (tablaSiembras ['Ciudad'] =="Tamesis")| (tablaSiembras ['Ciudad'] =="Yarumal")]
+#archivoHTML=tablaDatosxMunicipio.to_html()
+#archivoTEXTO=open("Datosxmunicipio.html","w",encoding="utf-8")
+#archivoTEXTO.write(archivoHTML) 
+#archivoTEXTO.close() 
+
+#Los datos de MEDELLIN ordenados de mayor a menor número de arboles
+#tablaDatos_Medellin=tablaSiembras[(tablaSiembras ['Ciudad'] =="Medellín")]
+#tablaDatos_Medellin=tablaDatos_Medellin.sort_values(by="Arboles")
+#archivoHTML=tablaDatos_Medellin.to_html()
+#archivoTEXTO=open("ArbolesMedellin.html","w",encoding="utf-8")
+#archivoTEXTO.write(archivoHTML) 
+#archivoTEXTO.close() 
+
+#Los datos de CAUCASIA incluyendo el número de hectáreas sembradas
+#tablaDatos_Caucasia=tablaSiembras[(tablaSiembras ['Ciudad'] =="Caucasia")]
+#archivoHTML=tablaDatos_Caucasia.to_html()
+#archivoTEXTO=open("Datos_Caucasia.html","w",encoding="utf-8")
+#archivoTEXTO.write(archivoHTML) 
+#archivoTEXTO.close() 
+
+# Los datos de Santa Fe de Antioquia donde se tengan siembras de >250 arboles
+#tablaDatos_Santafe=tablaSiembras[(tablaSiembras ['Ciudad'] =="Santa Fe de Antioquia") & (tablaSiembras ['Arboles'] > 250)]
+#archivoHTML=tablaDatos_Santafe.to_html()
+#archivoTEXTO=open("Datos_Santafe.html","w",encoding="utf-8")
+#archivoTEXTO.write(archivoHTML) 
+#archivoTEXTO.close() 
+
+#Los datos de las veredas Rio Arriba y la Salazar de Belmira
+
+#tablaDatos_Veredad_Rio=tablaSiembras[(tablaSiembras ['Vereda'] =="Rio Arriba")|(tablaSiembras ['Vereda'] =="La Salazar")]
+#archivoHTML=tablaDatos_Veredad_Rio.to_html()
+#archivoTEXTO=open("Datos_veredas_Rio.html","w",encoding="utf-8")
+#archivoTEXTO.write(archivoHTML) 
+#archivoTEXTO.close() 
+
+#Los datos de la veredas Quitasol de BELLO ordenados de menor a mayor y el promedio de arboles sembrados en esta vereda
+tablaDatos_Bello=tablaSiembras[(tablaSiembras ['Ciudad'] =="Bello") & (tablaSiembras ['Vereda'] =="Quitasol")]
+tablaDatos_Bello = tablaDatos_Bello.sort_values (by="Arboles")
+tablaDatos_Bello=tablaDatos_Bello.sort_values(median="Arboles")
+print(tablaDatos_Bello)
+#archivoHTML=tablaDatos_Bello.to_html()
+#archivoTEXTO=open("Datos_Bello.html","w",encoding="utf-8")
+#archivoTEXTO.write(archivoHTML) 
+#archivoTEXTO.close() 
+
+
+
+#Todos los datos de CARAMANTA
+
+#tablaDatos_caramanta=tablaSiembras[(tablaSiembras ['Ciudad'] =="Caramanta")]
+#archivoHTML=tablaDatos_caramanta.to_html()
+#archivoTEXTO=open("Datos_Caramanta.html","w",encoding="utf-8")
+#archivoTEXTO.write(archivoHTML) 
+#archivoTEXTO.close() 
+
+
+
 
 
 
